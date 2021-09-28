df57228ee8f77149c31cc180c8d2131687863355
Jorge Martinez <jorgemg0327@gmail.com>
2021-09-28 14:58:59 -0500

    Move mapathon reports to separate router

--- osmstats/changesets/__init__.py
+++ osmstats/changesets/__init__.py
@@ -7,46 +7,6 @@ from datetime import datetime, date, timedelta
 
 from .. import BaseModel
 
-
-class InsightFeature(BaseModel):
-    feature: str
-    action: str
-    count: int
-
-
-class InsightReport(BaseModel):
-    total_contributors: int
-    mapped_features: List[InsightFeature]
-
-
-class InsightsParams(BaseModel):
-    project_ids: List[int]
-    from_timestamp: Union[datetime, date]
-    to_timestamp: Union[datetime, date]
-    hashtags: List[str]
-
-    @validator("to_timestamp")
-    def check_timestamp_diffs(cls, value, values, **kwargs):
-        from_timestamp = values.get("from_timestamp")
-        timestamp_diff = value - from_timestamp
-        if timestamp_diff > timedelta(hours=24):
-            raise ValueError(
-                "Timestamp difference must be lower than 24 hours"
-            )
-
-        return value
-
-    @validator("hashtags")
-    def check_hashtag_filter(cls, value, values, **kwargs):
-        project_ids = values.get("project_ids")
-        if len(project_ids) == 0 and len(value) == 0:
-            raise ValueError(
-                "Empty lists found for both hashtags and project_ids params"
-            )
-
-        return value
-
-
 class PolygonFilter(Enum):
     iso3 = "iso3"
     geojson = "geojson"
--- osmstats/changesets/routers.py
+++ osmstats/changesets/routers.py
@@ -4,84 +4,11 @@ from psycopg2 import sql
 from psycopg2.extras import DictCursor
 
 from .. import config
-from . import (
-    FilterParams,
-    ChangesetResult,
-    InsightsParams,
-    InsightReport,
-    InsightFeature,
-)
+from . import ChangesetResult, FilterParams
 from .utils import geom_filter_subquery
 from fastapi import APIRouter
 
 router = APIRouter(prefix="/changesets")
-
-
-@router.post("/features/count", response_model=InsightReport)
-def count_features(params: InsightsParams):
-    hstore_column = "tags"
-    hashtag_project_ids = [f"%hotosm-project-{i}%" for i in params.project_ids]
-    hashtag_filter_values = [
-        *hashtag_project_ids,
-        *[f"%{h}%" for h in params.hashtags],
-    ]
-
-    hstore_keys = ["hashtags", "comment"]
-    timestamp_column = "created_at"
-
-    db_params = dict(config.items("INSIGHTS_PG"))
-    conn = psycopg2.connect(**db_params)
-
-    cur = conn.cursor(cursor_factory=DictCursor)
-
-    # Subquery to filter changesets matching hashtag and dates.
-    hashtag_filter_query = "({hstore_column} -> %s) ~~ %s"
-    hashtag_filter = [
-        [
-            cur.mogrify(hashtag_filter_query, (h, i)).decode()
-            for h in hstore_keys
-        ]
-        for i in hashtag_filter_values
-    ]
-
-    # Flatten.
-    hashtag_filter = [item for sublist in hashtag_filter for item in sublist]
-
-    hashtag_filter = [
-        sql.SQL(f).format(hstore_column=sql.Identifier(hstore_column))
-        for f in hashtag_filter
-    ]
-
-    hashtag_filter = sql.SQL(" OR ").join(hashtag_filter).as_string(conn)
-
-    timestamp_filter = sql.SQL("{timestamp_column} between %s AND %s").format(
-        timestamp_column=sql.Identifier(timestamp_column)
-    )
-    timestamp_filter = cur.mogrify(
-        timestamp_filter, (params.from_timestamp, params.to_timestamp)
-    ).decode()
-
-    changeset_query = f"SELECT id FROM osm_changeset WHERE {timestamp_filter} AND ({hashtag_filter})"
-
-    # OSM element history query.
-    osm_history_query = f"SELECT (each({hstore_column})).key AS feature, action, count(distinct id) AS count FROM osm_element_history where changeset in ({changeset_query}) group by feature, action ORDER BY count desc;"
-
-    cur.execute(osm_history_query)
-    result = cur.fetchall()
-
-    mapped_features = [InsightFeature(**r) for r in result]
-
-    # Get total contributors.
-    cur.execute(
-        f"SELECT COUNT(distinct user_id) as contributors_count from osm_changeset where {timestamp_filter} AND ({hashtag_filter})"
-    )
-    total_contributors = cur.fetchone().get("contributors_count")
-
-    report = InsightReport(
-        total_contributors=total_contributors, mapped_features=mapped_features
-    )
-
-    return report
 
 
 @router.post("/", response_model=ChangesetResult)
