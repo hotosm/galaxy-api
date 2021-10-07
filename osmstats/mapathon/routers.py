5749e04db99bea6241b188053b681ccfd52426aa
Jorge Martinez <jorgemg0327@gmail.com>
2021-10-07 14:58:32 +0200

    Refactor routers into functions

--- osmstats/mapathon/routers.py
+++ osmstats/mapathon/routers.py
@@ -1,65 +1,82 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 import psycopg2
 
-from psycopg2 import sql
 from psycopg2.extras import DictCursor
 
 from .. import config
 
-from . import MappedFeature, MapathonSummary, MapathonRequestParams
+from . import (
+    MappedFeature,
+    MapathonSummary,
+    MapathonRequestParams,
+    MappedFeatureWithUser,
+    MapathonContributor,
+    MapathonDetail,
+)
+
+from .utils import (
+    create_changeset_query,
+    create_osm_history_query,
+    create_users_contributions_query,
+)
+
+from ..auth import login_required
 
 router = APIRouter(prefix="/mapathon")
 
 
-@router.post("/summary", response_model=MapathonSummary)
-def count_features(params: MapathonRequestParams):
-    hstore_column = "tags"
-    hashtag_project_ids = [f"%hotosm-project-{i}%" for i in params.project_ids]
-    hashtag_filter_values = [
-        *hashtag_project_ids,
-        *[f"%{h}%" for h in params.hashtags],
-    ]
-
-    hstore_keys = ["hashtags", "comment"]
-    timestamp_column = "created_at"
-
+@router.post("/detail", response_model=MapathonDetail)
+def get_mapathon_detailed_report(
+    params: MapathonRequestParams, user_data=Depends(login_required)
+):
     db_params = dict(config.items("INSIGHTS_PG"))
     conn = psycopg2.connect(**db_params)
 
     cur = conn.cursor(cursor_factory=DictCursor)
 
-    # Subquery to filter changesets matching hashtag and dates.
-    hashtag_filter_query = "({hstore_column} -> %s) ~~ %s"
-    hashtag_filter = [
-        [
-            cur.mogrify(hashtag_filter_query, (h, i)).decode()
-            for h in hstore_keys
-        ]
-        for i in hashtag_filter_values
-    ]
-
-    # Flatten.
-    hashtag_filter = [item for sublist in hashtag_filter for item in sublist]
-
-    hashtag_filter = [
-        sql.SQL(f).format(hstore_column=sql.Identifier(hstore_column))
-        for f in hashtag_filter
-    ]
-
-    hashtag_filter = sql.SQL(" OR ").join(hashtag_filter).as_string(conn)
-
-    timestamp_filter = sql.SQL("{timestamp_column} between %s AND %s").format(
-        timestamp_column=sql.Identifier(timestamp_column)
-    )
-    timestamp_filter = cur.mogrify(
-        timestamp_filter, (params.from_timestamp, params.to_timestamp)
-    ).decode()
-
-    changeset_query = f"SELECT id FROM osm_changeset WHERE {timestamp_filter} AND ({hashtag_filter})"
+    changeset_query, _, _ = create_changeset_query(params, conn, cur)
 
     # OSM element history query.
-    osm_history_query = f"SELECT (each({hstore_column})).key AS feature, action, count(distinct id) AS count FROM osm_element_history where changeset in ({changeset_query}) group by feature, action ORDER BY count desc;"
+    osm_history_query = create_osm_history_query(
+        changeset_query, with_username=True
+    )
+
+    cur.execute(osm_history_query)
+    result = cur.fetchall()
+
+    mapped_features = [MappedFeatureWithUser(**r) for r in result]
+
+    contributors_query = create_users_contributions_query(
+        params, changeset_query
+    )
+    cur.execute(contributors_query)
+    result = cur.fetchall()
+
+    contributors = [MapathonContributor(**r) for r in result]
+
+    report = MapathonDetail(
+        contributors=contributors, mapped_features=mapped_features
+    )
+
+    return report
+
+
+@router.post("/summary", response_model=MapathonSummary)
+def get_mapathon_summary(params: MapathonRequestParams):
+    db_params = dict(config.items("INSIGHTS_PG"))
+    conn = psycopg2.connect(**db_params)
+
+    cur = conn.cursor(cursor_factory=DictCursor)
+
+    changeset_query, hashtag_filter, timestamp_filter = create_changeset_query(
+        params, conn, cur
+    )
+
+    # OSM element history query.
+    osm_history_query = create_osm_history_query(
+        changeset_query, with_username=False
+    )
 
     cur.execute(osm_history_query)
     result = cur.fetchall()
@@ -68,7 +85,11 @@ def count_features(params: MapathonRequestParams):
 
     # Get total contributors.
     cur.execute(
-        f"SELECT COUNT(distinct user_id) as contributors_count from osm_changeset where {timestamp_filter} AND ({hashtag_filter})"
+        f"""
+        SELECT COUNT(distinct user_id) as contributors_count
+        FROM osm_changeset
+        WHERE {timestamp_filter} AND ({hashtag_filter})
+    """
     )
     total_contributors = cur.fetchone().get("contributors_count")
 
