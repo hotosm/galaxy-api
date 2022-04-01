9f4b1959348ab220d00b69808edbd644bb7a7644
eternaltyro <eternaltyro@protonmail.ch>
2022-04-01 09:26:51 +0530

    Fix configfile path

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo ${GALAXY_API_CONFIG_FILE} | base64 -d | gzip -d | tee /app/config.txt
+echo ${GALAXY_API_CONFIG_FILE} | base64 -d | gzip -d | tee /app/src/config.txt
 uvicorn API.main:app --reload --host 0.0.0.0 --port 8000
