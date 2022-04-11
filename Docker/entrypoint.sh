e24f94ca3642ba0bd0e5c0e2d82d0d6774770195
eternaltyro <eternaltyro@protonmail.ch>
2022-04-11 18:51:20 +0530

    Fix Dockerfile errors

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+if [[ -z "${GALAXY_API_CONFIG_FILE}" ]]; then
+    printf "Error: GALAXY_API_CONFIG_FILE environment variable missing.. Exiting!"
+    exit 1
+fi
 
 echo ${GALAXY_API_CONFIG_FILE} | base64 -d | gzip -d | tee /app/src/config.txt
-uvicorn API.main:app --reload --host 0.0.0.0 --port 8000
+uvicorn API.main:app --reload --host 0.0.0.0 --port 8000 --log-level error --no-use-colors
