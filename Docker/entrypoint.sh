988bfce7ab9fdc43207626f462384a0f33f0b93d
eternaltyro <eternaltyro@protonmail.ch>
2022-05-30 16:47:49 +0530

    Enable proxy-headers at uvicorn

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -6,4 +6,4 @@ if [[ -z "${GALAXY_API_CONFIG_FILE}" ]]; then
 fi
 
 echo ${GALAXY_API_CONFIG_FILE} | base64 -d | gzip -d | tee /app/src/config.txt
-uvicorn API.main:app --reload --host 0.0.0.0 --port 8000 --log-level error --no-use-colors
+uvicorn --proxy-headers --reload --host 0.0.0.0 --port 8000 --log-level error --no-use-colors API.main:app
