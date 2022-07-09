5016f843449a27bfd4c048b0064592a5236a983b
Kshitij Raj Sharma <skshitizraj@gmail.com>
2022-07-09 19:24:24 +0545

    removed usage of asyncpg and added threaded connection pooling to rawdata endpoint #252

--- src/galaxy/db_connection.py
+++ src/galaxy/db_connection.py
@@ -1,47 +1,84 @@
-import asyncpg
+# Copyright (C) 2021 Humanitarian OpenStreetmap Team
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Humanitarian OpenStreetmap Team
+# 1100 13th Street NW Suite 800 Washington, D.C. 20005
+# <info@hotosm.org>
+
 from .config import config
 import logging
-
+import psycopg2
+from psycopg2 import pool
 
 class Database:
+    """Handles the all work related to connection pooling
+    """
     def __init__(self):
         self.db_params = dict(config.items("RAW_DATA"))
         self._cursor = None
-        self._connection_pool = None
+        self.threaded_postgreSQL_pool  = None
         self.con = None
 
-    async def connect(self):
-        if not self._connection_pool:
+    def connect(self):
+        """Connection to the database 
+        """
+        if not self.threaded_postgreSQL_pool :
             try:
-                self._connection_pool = await asyncpg.create_pool(
-                    min_size=1,
-                    max_size=10,
-                    **self.db_params
-                )
+                #creating pool through psycopg2 with threaded connection so that there will support minimum 5 users to 20 users 
+                self.threaded_postgreSQL_pool = psycopg2.pool.ThreadedConnectionPool(5, 20,**self.db_params)
                 logging.debug("Connection pooling has been established")
-
+                if (self.threaded_postgreSQL_pool):
+                    logging.debug("Connection pool created successfully using ThreadedConnectionPool")
             except Exception as e:
                 logging.error(e)
+    
+    def get_conn_from_pool(self):
+        """Function to get connection from the pool instead of new connection
 
-    async def get_db_conn_from_pool(self):
+        Returns:
+            connection 
+        """
+        if (self.threaded_postgreSQL_pool):
+            # Use getconn() method to Get Connection from connection pool
+            pool_conn = self.threaded_postgreSQL_pool.getconn()
+            return pool_conn
+
+    async def release_conn_from_pool(self,pool_con):
+        """Can be used to release specific connection after its use from the pool , so that it can be used by another process
+
+        Args:
+            pool_con (_type_): define which connection to remove from pool
+
+        Raises:
+            ex: error if connection doesnot exists or misbehave of function
+        """
         try:
-            db_con = await self._connection_pool.acquire()
-            return db_con
-        except Exception as ex :
+            # Use this method to release the connection object and send back ti connection pool
+            await self.threaded_postgreSQL_pool.putconn(pool_con)
+            logging.debug("Throwing away postgresql connection from thread")
+
+        except Exception as ex:
             logging.error(ex)
             raise ex
+    
+    async def close_all_connection_pool(self):
+        """Closes the connection thread created by thread pooling all at once 
+        """
+        # closing database connection.
+        # use closeall() method to close all the active connection if you want to turn of the application
+        if self.threaded_postgreSQL_pool:
+            await self.threaded_postgreSQL_pool.closeall
+        logging.debug("Threaded PostgreSQL connection pool is closed")
 
-
-    async def fetch_rows(self, query: str):
-        logging.debug("fetch rows has been called")
-        if not self._connection_pool:
-            await self.connect()
-        else:
-            self.con = await self._connection_pool.acquire()
-            try:
-                result = await self.con.fetch(query)
-                return result
-            except Exception as e:
-                logging.error(e)
-            finally:
-                await self._connection_pool.release(self.con)
