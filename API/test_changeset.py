50f25748a22d4d5b3c7f31d0217410f4c47fbf21
itskshtiiz321 <skshitizraj@gmail.com>
2022-08-11 07:26:59 +0000

    added missing row info

--- API/test_changeset.py
+++ API/test_changeset.py
@@ -24,7 +24,7 @@ def test_changeset_difference(start_date,end_date,hashtag):
                                 or ("tags" -> 'hashtags') ~~* '%{hashtag}'
                                     or ("tags" -> 'comment') ~~* '%{hashtag}'))
                     select
-                        count(distinct id) as intersected_changeset_count
+                        count(distinct id) as intersected_changeset_count 
                     from
                         t1 ,
                         geoboundaries as g
@@ -32,30 +32,74 @@ def test_changeset_difference(start_date,end_date,hashtag):
                         ST_Intersects(t1.geom ,
                         ST_SetSRID(g.boundary,
                         4326));"""
+    insight_query_changeset_id_list=f"""with t1 as (
+                    select
+                        geom ,
+                        id
+                    from
+                        osm_changeset
+                    where
+                        "created_at" between '{start_date}T00:00:00'::timestamp and '{end_date}T00:00:00'::timestamp
+                        and (("tags" -> 'hashtags') ~~* '%{hashtag};%'
+                            or ("tags" -> 'comment') ~~* '%{hashtag} %'
+                                or ("tags" -> 'hashtags') ~~* '%{hashtag}'
+                                    or ("tags" -> 'comment') ~~* '%{hashtag}'))
+                    select
+                        distinct id
+                    from
+                        t1 ,
+                        geoboundaries as g
+                    where
+                        ST_Intersects(t1.geom ,
+                        ST_SetSRID(g.boundary,
+                        4326));"""                 
+                        
     underpass_query=f"""select
-                            COUNT(distinct id) as total_changeset_count
+                            COUNT(distinct id) as total_changeset_found_with_hashtag
                         from
                             changesets
                         where
                             ("created_at" between '{start_date}T00:00:00'::timestamp and '{end_date}T00:00:00'::timestamp)
                             and ('{hashtag}' ~~* any(hashtags))"""
-    underpass = Database(get_db_connection_params("UNDERPASS"))
-    underpass.connect()
-    print(underpass_query)
-    
-    underpass_result=underpass.executequery(underpass_query)
-    underpass.close_conn()
-    
     insight=Database(get_db_connection_params("INSIGHTS"))
     insight.connect()
-    print(insight_query)
+    # print(insight_query)
     
     insight_result=insight.executequery(insight_query)
+    # print(insight_query_changeset_id_list)
+    insight_changeset_list=insight.executequery(insight_query_changeset_id_list)
+
     insight.close_conn()
+    
+    changeset_list_within_proiority_insight=[]
+    for r in insight_changeset_list:
+        changeset_list_within_proiority_insight.append(f"""id={r[0]}""")
+    filter_changeset_list = " or ".join(changeset_list_within_proiority_insight)
+    underpass_changeset_check_query=f"""select
+                            COUNT(distinct id) as total_changeset_found_with_id
+                        from
+                            changesets
+                        where
+                            {filter_changeset_list}"""
+    # print(underpass_changeset_check_query)
+    underpass = Database(get_db_connection_params("UNDERPASS"))
+    underpass.connect()
+    # print(underpass_query)
+    
+    underpass_result=underpass.executequery(underpass_query)
+    underpass_result_for_id=underpass.executequery(underpass_changeset_check_query)
+    
+    underpass.close_conn()
+    
+
     
 
     return {
-        "insight" : insight_result[0][0],
-        "underpass" : underpass_result[0][0],
-        "difference" :insight_result[0][0]-underpass_result[0][0]
+        "insight_changeset_intersected_priority_count" : insight_result[0][0],
+        "underpass_changeset_count_#" : underpass_result[0][0],
+        "underpass_changeset_count_id" : underpass_result_for_id[0][0],
+        "changeset_with_metadata_missing" :insight_result[0][0]-underpass_result[0][0],
+        "whole_changeset_row_missing" :insight_result[0][0]- underpass_result_for_id[0][0],
+        
+        
     }
