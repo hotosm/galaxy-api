d31e90c62ff6500a38e144e231ce84ca16e85a4e
NicolasGrosjean <grosjen@hotmail.fr>
2022-09-19 14:56:09 +0200

    Test osm-users/statistics/ endpoint

---
+++ tests/src/fixtures/insights2.sql
@@ -0,0 +1,4 @@
+COPY public.all_changesets_stats (changeset, added_buildings, modified_buildings, added_amenity, modified_amenity, added_highway, modified_highway, added_highway_meters, modified_highway_meters, added_places, modified_places) FROM stdin;
+60614568	0	0	0	0	1	2	64.71945829647886	0	0	0
+60614577	0	0	0	0	1	1	0	0	0	0
+\.
