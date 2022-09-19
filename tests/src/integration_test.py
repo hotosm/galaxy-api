cda6f1c8290fc1514bf02f5c70f20a1677e62456
NicolasGrosjean <grosjen@hotmail.fr>
2022-09-19 10:06:42 +0200

    Test only the structure instead of hardcoded values

--- tests/src/integration_test.py
+++ tests/src/integration_test.py
@@ -4,11 +4,24 @@ import os
 
 from API.main import app as fastapi_app
 
-EXPECTED_OUT_DIR = os.path.join("tests", "expected")
-
 def test_countries():
     client = TestClient(fastapi_app)
     response = client.get("/latest/countries")
     assert response.status_code == 200
-    with open(os.path.join(EXPECTED_OUT_DIR, "countries.json"), "r") as json_file:
-        assert response.json() == json.load(json_file)
+    res = response.json()
+    assert res["type"] == "FeatureCollection"
+    assert len(res["features"]) > 0
+    for feature in res["features"]:
+        assert feature["type"] == "Feature"
+        assert type(feature["properties"]["name"]) == str
+        print(feature["properties"]["name"])
+        assert feature["geometry"]["type"] == "MultiPolygon"
+        coordinates = feature["geometry"]["coordinates"]
+        assert len(coordinates) == 1
+        assert len(coordinates[0]) > 0
+        for i in range(len(coordinates[0])):
+            assert len(coordinates[0][i]) > 0
+            for point in coordinates[0][i]:
+                assert len(point) == 2
+                assert type(point[0]) == float
+                assert type(point[1]) == float
