226911a547e0ffa8a0eab539254ea4cf56e3da85
NicolasGrosjean <grosjen@hotmail.fr>
2022-09-19 10:57:03 +0200

    Test osm-users/ids/ endpoint

--- tests/src/integration_test.py
+++ tests/src/integration_test.py
@@ -1,11 +1,11 @@
 from fastapi.testclient import TestClient
-import json
-import os
 
 from API.main import app as fastapi_app
 
+client = TestClient(fastapi_app)
+usernames = ["mspray6", "mspray7"]
+
 def test_countries():
-    client = TestClient(fastapi_app)
     response = client.get("/latest/countries")
     assert response.status_code == 200
     res = response.json()
@@ -25,3 +25,34 @@ def test_countries():
                 assert len(point) == 2
                 assert type(point[0]) == float
                 assert type(point[1]) == float
+
+def test_osm_users_ids_OK():
+    response = client.post("/latest/osm-users/ids/", json={"userNames":usernames,"fromTimestamp":"2018-07-11T00:00:00Z","toTimestamp":"2018-07-12T00:00:00Z"})
+    assert response.status_code == 200
+    res = response.json()
+    assert len(res) == len(usernames)
+    for i in range(len(res)):
+        assert type(res[i]["userId"]) == int
+        assert res[i]["userName"] == usernames[i]
+
+def test_osm_users_ids_old_date():
+    response = client.post("/latest/osm-users/ids/", json={"userNames":usernames,"fromTimestamp":"2000-07-11T00:00:00Z","toTimestamp":"2000-07-12T00:00:00Z"})
+    assert response.status_code == 200
+    assert len(response.json()) == 0
+
+def test_osm_users_ids_no_user():
+    response = client.post("/latest/osm-users/ids/", json={"userNames":[],"fromTimestamp":"2018-07-11T00:00:00Z","toTimestamp":"2018-07-12T00:00:00Z"})
+    assert response.status_code == 200
+    assert len(response.json()) == 0
+
+def test_osm_users_ids_missing_param():
+    response = client.post("/latest/osm-users/ids/", json={"fromTimestamp":"2018-07-11T00:00:00Z","toTimestamp":"2018-07-12T00:00:00Z"})
+    assert response.status_code == 422
+    response = client.post("/latest/osm-users/ids/", json={"userNames":usernames,"toTimestamp":"2018-07-12T00:00:00Z"})
+    assert response.status_code == 422
+    response = client.post("/latest/osm-users/ids/", json={"userNames":usernames,"fromTimestamp":"2018-07-11T00:00:00Z"})
+    assert response.status_code == 422
+
+def test_osm_users_ids_bad_timestamp_order():
+    response = client.post("/latest/osm-users/ids/", json={"userNames":usernames,"fromTimestamp":"2018-07-11T00:00:00Z","toTimestamp":"2018-07-10T00:00:00Z"})
+    assert response.status_code == 422
